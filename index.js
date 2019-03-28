aee324aa2ccb451998b6f1a5de4eaeacb476ceed
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-03-27 18:49:25 -0700

    added cohorts router file, just need to move routes over to the router file.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const knex = require('knex');
 const knexConfig = require('./knexfile');
 
 const db = knex(knexConfig.development);
-// const cohortsRouter = 
+const cohortsRouter = require('./routers/cohortsRouter');
+// const studentsRouter = require('./routers/students');
 
 server.use(helmet());
 server.use(express.json());
@@ -14,7 +15,7 @@ server.get('/', (req, res) => {
   res.send("Server Working!");
 })
 
-// server.use('/api/cohorts', cohortsRouter);
+server.use('/api/cohorts', cohortsRouter);
 
 server.get('/api/cohorts', async (req, res) => {
   try {
@@ -24,6 +25,58 @@ server.get('/api/cohorts', async (req, res) => {
     res.status(500).json({ message: error });
   }
 })
+
+server.get('/api/cohorts/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const cohort = await db('cohorts').where({ id: id }).first();
+    res.status(200).json(cohort);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+})
+
+server.post('/api/cohorts', async (req, res) => {
+  if (!req.body) {
+    return (res.status(400).json({ message: "Please fill in the name of the new cohort" }))
+  }
+  try {
+    const ids = await db('cohorts').insert(req.body);
+    const id = ids[0];
+    const cohort = await db('cohorts').where({ id }).first()
+    res.status(201).json(cohort);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
+
+server.put('/api/cohorts/:id', async (req, res) => {
+  try {
+    const count = await db('cohorts').where({ id: req.params.id }).update(req.body);
+    if (count > 0) {
+      const cohort = await db('cohorts').where({ id: req.params.id }).first();
+      res.status(200).json({ cohort })
+    } else {
+      res.status(404).json({ message: "Cohort not found. Please try another" })
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
+
+server.delete('/api/cohorts/:id', async (req, res) => {
+  try {
+    const count = await db('cohorts').where({ id: req.params.id }).del();
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "404 error: Record not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ messsage: "Internal server error" });
+  }
+})
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
